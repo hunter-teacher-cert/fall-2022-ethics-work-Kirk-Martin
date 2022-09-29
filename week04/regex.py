800b7772de1631d45c88ddaf274884146ccb2bd6
Kirk-Martin <108408820+Kirk-Martin@users.noreply.github.com>
2022-09-28 20:24:05 -0400

    Add files via upload

--- week04/regex.py
+++ week04/regex.py
@@ -1,18 +1,50 @@
+#Kirk Martin
+#Ethics Class
+#Sept 27th, 2022
+# Week 4 regular expressions
+
+
 import re
 
 
-def find_date(line):
-    pattern = r"\d{1,2}/\d{1,2}/\d{2,4}"
+def find_name(line):
+ 
+    #First Name Last Name Expression Works
+    pattern=r'[A-Z]\w*\s[A-Z]\w*'
     result = re.findall(pattern,line)
 
-    pattern=r'(October|Oct|November|Nov)( [0-9]{1,2}, [0-9]{4})'
+    #First Name Middle Name and Last name expression
+    pattern=r'[A-Z]\w*\s[A-Z]\w*\s[A-Z]\w*'
+    result = result + re.findall(pattern,line)
+
+    #F. M. Last Works
+    pattern=r'[A-Z]\.\s[A-Z]\.\s[A-Z]\w*'
+    result = result + re.findall(pattern,line)
+    
+    #Mr. Last attempt at expression
+    pattern=r'Mr.\s[A-Z]\w*'
+    result = result + re.findall(pattern,line)
+
+    #Dr. and Last name appear in list.
+    pattern=r'Dr.\s[A-Z]\w*'
+    result = result + re.findall(pattern,line)
+
+    #Mrs. Last expression
+    pattern=r'Mrs.\s[A-Z]\w*'
+    result = result + re.findall(pattern,line)
+
+    #Ms. F. Last expression
+    pattern=r'Ms.\s[A-Z]\.\s[A-Z]\w*'
+    result = result + re.findall(pattern,line)
+
+    #Mrs. F. Last name expression
+    pattern=r'Mrs.\s[A-Z]\.\s[A-Z]\w*'
     result = result + re.findall(pattern,line)
     return result
-
 
 f = open("Names.txt")
 for line in f.readlines():
     #print(line)
-    result = find_date(line)
+    result = find_name(line)
     if (len(result)>0):
         print(result)
